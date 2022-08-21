d6b4f6ba87b6929a5aa73a92dd08612e411230fb
Marina <marinosharipo@gmail.com>
2022-08-21 23:15:57 +0700

    added quad of number

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number * number;
+
+
+Console.Write($"Квадрат числа {number} = {kvadrat}.");
