6b946609dba36f737d87365cb19a0b091d79f599
Marina <marinosharipo@gmail.com>
2022-08-25 10:29:56 +0700

    added example001

---
+++ Example01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Введите A: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine("A больше B");
+}
+else
+{
+    if(number1 < number2)
+    {
+      Console.WriteLine("B больше A");  
+    }
+    else
+    {
+       Console.WriteLine("A равно B"); 
+    }
+}
--- task1/Program.cs
+++ task1/Program.cs
@@ -1,7 +1,15 @@
-﻿Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿
+// Задача 1. Вывод квадрата введенного числа.
 
-int kvadrat = number * number;
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int kvadrat = number * number;
 
 
-Console.Write($"Квадрат числа {number} = {kvadrat}.");
+// Console.Write($"Квадрат числа {number} = {kvadrat}.");
+
+// Задача 2. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Write("Введите число 1: ");
+int number_1 = Convert.ToInt32
