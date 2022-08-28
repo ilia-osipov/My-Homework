a4dfb17418c51542eae279040cef967ffb86fe74
Marina <marinosharipo@gmail.com>
2022-08-28 17:39:19 +0700

    added HM3

---
+++ Example2_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(num)-2;
+if(k < 0)
+{
+    Console.WriteLine("третьей цифры нет");  
+    return;
+}
+    else
+{
+    Console.WriteLine(k < 0 ? "третьей цифры нет" : (num % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+}
+
+
