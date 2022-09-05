0ae2d740283a53d25733092c97c86cddc1d88449
Marina <marinosharipo@gmail.com>
2022-09-05 22:25:10 +0700

    added

---
+++ Example04_03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+int[] array = GetArray(8);
+
+Console.WriteLine("Массив из 8 элементов: ");
+
+Console.WriteLine($"[{string.Join(",",array)}]");
+
+int[] GetArray(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+
+    return arr;
+}
---
+++ Example4_01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("введите основание");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите степень");
+int b = int.Parse(Console.ReadLine());
+int Result(int i)
+{
+    int res = a;
+    for ( i=1; i<b; i++)
+    {
+        res *= a;
+    }
+    return res;
+}
+Console.WriteLine(Result(b));
